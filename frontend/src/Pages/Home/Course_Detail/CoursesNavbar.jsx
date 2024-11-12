5dc13204b35a85912e5a8cd4e683b2fb88550836
Abhicoder786 <117636060+abhiCoderShek@users.noreply.github.com>
2024-11-12 15:21:43 +0530

    updatesecond

--- frontend/src/Pages/Home/Course_Detail/CoursesNavbar.jsx
+++ frontend/src/Pages/Home/Course_Detail/CoursesNavbar.jsx
@@ -72,7 +72,7 @@ export default function CoursesNavbar() {
         <div className='m-auto   mt-16' style={{maxWidth:'1400px'}}>
         <Swiper modules={[ Autoplay, Thumbs,  Navigation]}  followFinger={true} grabCursor={true} simulateTouch={true} spaceBetween={30} breakpoints = {{469: {slidesPerView: 1}, 768: {slidesPerView: 2}, 1240: {slidesPerView:3}}} 
          autoplay={{ delay: 3000,  disableOnInteraction: false  }}   className="w-full  h-fit mt-6  z-0  max-lg:pl-6 max-lg:pr-6 "> 
-           <SwiperSlide className='  border' >
+           <SwiperSlide className=' ' >
     
             {/*Selected Feild Course 1 */}
                 <div className='border flex rounded-2xl  max-lg:h-fit max-md:h-60 max-lg:pb-3 max-md:pb-0 ' style={{maxwidth: '100%',backgroundColor:'#EDEDED'}}>
@@ -88,7 +88,7 @@ export default function CoursesNavbar() {
                             <p>Lorem ipsum dolor sit amet , adipisi elit. Reprehenderit, inventore debitis temporibus nihil optio, ullam debitis temporibus nihil optio, ullam
                             </p>
                         </div>
-                        <div className='flex justify-end 'style={{marginTop:'20px'}}>
+                        <div className='flex justify-end  mt-5 max-lg:mt-10 max-md:mt-5 max-sm:mt-0'>
                             <div>
                                 <button className='w-20 rounded-full text-center text-black font-semibold bg-purple-300 mr-3' style={{height:'36px',fontSize:'15px',fontWeight:'600'}}>Preview</button>
                             </div>
@@ -101,12 +101,12 @@ export default function CoursesNavbar() {
 
             {/*Selected Feild Course 2 */}
            </SwiperSlide>
-           <SwiperSlide className='  border' >
+           <SwiperSlide className='  ' >
     
     {/*Selected Feild Course 1 */}
-        <div className='border flex rounded-2xl h-60 max-lg:h-fit max-md:h-60 max-lg:pb-3 max-md:pb-0' style={{maxwidth: '100%',backgroundColor:'#EDEDED'}}>
-            <div className='max-w-1/3 h-full '>
-                <img src={CourseImage} className='w-full h-full rounded-tl-2xl rounded-bl-2xl' alt="" />
+        <div className='border flex rounded-2xl  max-lg:h-fit max-md:h-60 max-lg:pb-3 max-md:pb-0 ' style={{maxwidth: '100%',backgroundColor:'#EDEDED'}}>
+            <div className='max-w-1/3 h-full  max-md:h-60 border '>
+                <img src={CourseImage} className='w-full h-full  max-md:h-60 rounded-tl-2xl rounded-bl-2xl' alt="" />
             </div>
                 {/* details */}
             <div className='w-2/3 h-full'>
@@ -114,10 +114,10 @@ export default function CoursesNavbar() {
                     <p className='text-xl font-bold  '>Java</p>
                 </div>
                 <div className='ml-6 mt-2 mr-3 mb-1 'style={{color:'#49454F'}}>
-                    <p className=' max-lg:overflow-y-auto'>Lorem ipsum dolor sit amet , adipisi elit. Reprehenderit, inventore debitis temporibus nihil optio, ullam debitis temporibus nihil optio, ullam
+                    <p>Lorem ipsum dolor sit amet , adipisi elit. Reprehenderit, inventore debitis temporibus nihil optio, ullam debitis temporibus nihil optio, ullam
                     </p>
                 </div>
-                <div className='flex justify-end 'style={{marginTop:'20px'}}>
+                <div className='flex justify-end  mt-5 max-lg:mt-10  max-md:mt-5 border max-sm:mt-0'>
                     <div>
                         <button className='w-20 rounded-full text-center text-black font-semibold bg-purple-300 mr-3' style={{height:'36px',fontSize:'15px',fontWeight:'600'}}>Preview</button>
                     </div>
@@ -129,8 +129,8 @@ export default function CoursesNavbar() {
         </div >
 
     {/*Selected Feild Course 2 */}
-          </SwiperSlide>
-           
+   </SwiperSlide>
+         
         </Swiper>
         </div>
     </div>
