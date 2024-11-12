f6af07de3672ff5b85702f89e23153211ec60278
Abhicoder786 <117636060+abhiCoderShek@users.noreply.github.com>
2024-11-12 15:39:32 +0530

    first update

--- frontend/src/Pages/Home/Course_Detail/CoursesNavbar.jsx
+++ frontend/src/Pages/Home/Course_Detail/CoursesNavbar.jsx
@@ -20,7 +20,7 @@ export default function CoursesNavbar() {
     }
   return (
     <div>
-      <div className='w-full mt-12'>
+      <div className='w-full mt-12 '>
             <div className={`w-11/12 h-10 flex items-center  ${show?'justify-between':'justify-evenly'} pl-3 m-auto rounded-full`} style={{boxShadow:' rgba(9, 30, 66, 0.25) 0px 1px 1px, rgba(9, 30, 66, 0.13) 0px 0px 1px 1px'}}>
                 {/* Nav Feilds */}
                 {(!show && <div className='Nav-Fields flex justify-between  text-gray-600 font-bold flex-nowrap whitespace-nowrap ' style={{fontSize:'16px'}}>
@@ -72,25 +72,25 @@ export default function CoursesNavbar() {
         <div className='m-auto   mt-16' style={{maxWidth:'1400px'}}>
         <Swiper modules={[ Autoplay, Thumbs,  Navigation]}  followFinger={true} grabCursor={true} simulateTouch={true} spaceBetween={30} breakpoints = {{469: {slidesPerView: 1}, 768: {slidesPerView: 2}, 1240: {slidesPerView:3}}} 
          autoplay={{ delay: 3000,  disableOnInteraction: false  }}   className="w-full  h-fit mt-6  z-0  max-lg:pl-6 max-lg:pr-6 "> 
-           <SwiperSlide className=' ' >
+           <SwiperSlide className='h-fit ' >
     
             {/*Selected Feild Course 1 */}
-                <div className='border flex rounded-2xl  max-lg:h-fit max-md:h-60 max-lg:pb-3 max-md:pb-0 ' style={{maxwidth: '100%',backgroundColor:'#EDEDED'}}>
+                <div className='border flex rounded-2xl h-60  max-lg:h-fit max-md:h-60 max-lg:pb-3 max-md:pb-0 ' style={{maxwidth: '100%',backgroundColor:'#EDEDED'}}>
                     <div className='max-w-1/3 h-full  max-md:h-60 border '>
                         <img src={CourseImage} className='w-full h-full  max-md:h-60 rounded-tl-2xl rounded-bl-2xl' alt="" />
                     </div>
                         {/* details */}
-                    <div className='w-2/3 h-full'>
+                    <div className='w-2/3 h-full border'>
                         <div className='ml-6 mt-2.5'>
                             <p className='text-xl font-bold  '>Java</p>
                         </div>
                         <div className='ml-6 mt-2 mr-3 mb-1 'style={{color:'#49454F'}}>
-                            <p>Lorem ipsum dolor sit amet , adipisi elit. Reprehenderit, inventore debitis temporibus nihil optio, ullam debitis temporibus nihil optio, ullam
+                            <p className=' max-sm:text-xs'>Lorem ipsum dolor sit amet , adipisi elit. Reprehenderit, inventore debitis temporibus nihil optio, ullam debitis temporibus nihil optio, ullam
                             </p>
                         </div>
-                        <div className='flex justify-end  mt-5 max-lg:mt-10 max-md:mt-5 max-sm:mt-0'>
+                        <div className='flex justify-end  mt-5 max-lg:mt-10 max-md:mt-5 max-sm:mt-0 max-sm:p-3 '>
                             <div>
-                                <button className='w-20 rounded-full text-center text-black font-semibold bg-purple-300 mr-3' style={{height:'36px',fontSize:'15px',fontWeight:'600'}}>Preview</button>
+                                <button className='w-20   rounded-full text-center text-black font-semibold bg-purple-300 mr-3' style={{height:'36px',fontSize:'15px',fontWeight:'600'}}>Preview</button>
                             </div>
                             <div>
                                 <button className='w-24 rounded-full text-center text-white bg-purple-700 mr-4'style={{height:'36px',fontSize:'15px',fontWeight:'600'}}>Buy Now</button>
@@ -101,36 +101,7 @@ export default function CoursesNavbar() {
 
             {/*Selected Feild Course 2 */}
            </SwiperSlide>
-           <SwiperSlide className='  ' >
-    
-    {/*Selected Feild Course 1 */}
-        <div className='border flex rounded-2xl  max-lg:h-fit max-md:h-60 max-lg:pb-3 max-md:pb-0 ' style={{maxwidth: '100%',backgroundColor:'#EDEDED'}}>
-            <div className='max-w-1/3 h-full  max-md:h-60 border '>
-                <img src={CourseImage} className='w-full h-full  max-md:h-60 rounded-tl-2xl rounded-bl-2xl' alt="" />
-            </div>
-                {/* details */}
-            <div className='w-2/3 h-full'>
-                <div className='ml-6 mt-2.5'>
-                    <p className='text-xl font-bold  '>Java</p>
-                </div>
-                <div className='ml-6 mt-2 mr-3 mb-1 'style={{color:'#49454F'}}>
-                    <p>Lorem ipsum dolor sit amet , adipisi elit. Reprehenderit, inventore debitis temporibus nihil optio, ullam debitis temporibus nihil optio, ullam
-                    </p>
-                </div>
-                <div className='flex justify-end  mt-5 max-lg:mt-10  max-md:mt-5 border max-sm:mt-0'>
-                    <div>
-                        <button className='w-20 rounded-full text-center text-black font-semibold bg-purple-300 mr-3' style={{height:'36px',fontSize:'15px',fontWeight:'600'}}>Preview</button>
-                    </div>
-                    <div>
-                        <button className='w-24 rounded-full text-center text-white bg-purple-700 mr-4'style={{height:'36px',fontSize:'15px',fontWeight:'600'}}>Buy Now</button>
-                    </div>
-                </div>
-            </div>
-        </div >
-
-    {/*Selected Feild Course 2 */}
-   </SwiperSlide>
-         
+          
         </Swiper>
         </div>
     </div>
